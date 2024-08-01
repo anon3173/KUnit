c58161d50e295d2644700e50466dfe5881cfcf17
anon3173 <a31734190@gmail.com>
2024-08-01 14:16:19 -0500

    Add files via upload

--- Benchmark_Empirical_Evaluation/DeepLearningZeroToAll/keras/klab-04-1-multi_input_linear_regression_data.py
+++ Benchmark_Empirical_Evaluation/DeepLearningZeroToAll/keras/klab-04-1-multi_input_linear_regression_data.py
@@ -1,7 +1,6 @@
 
 
 from sklearn import preprocessing
-from sklearn.datasets import load_iris
 from tensorflow.keras.utils import to_categorical
 from keras.utils import np_utils 
 import pandas
@@ -12,13 +11,13 @@ import numpy as np
 import random
 import seaborn as sns
 from sklearn.datasets import make_regression, make_classification
-from sklearn.datasets import load_breast_cancer
+
 
 class DataPreprocess():
     
     def __init__(self):
         super(DataPreprocess, self).__init__()
-        # self.data_path = '/Users/ruchira/Desktop/FinalIRBupload/SO Posts/58055105/fixed.csv' # path for training data 
+        # self.data_path = '' # path for training data 
     
     def load(self):
         x_data = np.array([[73., 80., 75.],
